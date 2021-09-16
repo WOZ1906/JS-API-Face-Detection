44ff95248665e1d646ba64941e7e0a36ef74f6e6
Tristian Pitogo <freaxwoz@gmail.com>
2021-09-17 02:26:00 +0800

    facial node detection apply sucess

--- sketch.js
+++ sketch.js
@@ -6,17 +6,19 @@ let canvas;
 
 function setup() {
 canvas = createCanvas(480,360);
+canvas.id('canvas');
 video = createCapture (VIDEO);
+canvas.id('video');
 video.size(width, height);
 
-const faceOption = {
+const faceOptions = {
     withLandmarks: true,
     withExpressions: true,
     withDescriptors: false,
     minConfidence: 0.5
 };
 
-faceapi = ml5.faceApi(video, faceOptions,);
+faceapi = ml5.faceApi(video, faceOptions, faceReady);
 
 }
 
@@ -25,10 +27,47 @@ function faceReady () {
 }
 
 function gotFaces(error, result) {
-
-
+    if(error){
+        console.log(error);
+        return;
+    }
+    detections = result;
+    console.log(detections);
+    faceapi.detect(gotFaces);
 }
 
 function draw() {
-
+    clear();
+    drawBoxes(detections);
+    drawLandmarks(detections);  
 }
+
+function drawBoxes(detections) {
+    if(detections.length > 0) {
+        for(let f=0; f < detections.length; f++) {
+            let {_x, _y, _width, _height} = detections[0].alignedRect._box;
+            stroke(44, 169, 225);
+            strokeWeight(1);
+            noFill();
+            rect(_x, _y, _width, _height);
+        }
+    }
+}
+
+function drawLandmarks(detections) {
+    if(detections.length > 0) {
+        for(f=0; f<detections.length; f++) {
+            let points = detections[f].landmarks.positions;
+            for(let i=0; i<points.length; i++) {
+                stroke(44, 169, 225);
+                strokeWeight(3);
+                point(points[i]._x, points[i]._y);
+            }
+        }
+            
+        }
+    }
+
+    function drawExpressions(detections, x, y, textYspace) {
+        
+    }
